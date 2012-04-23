3b3a8eabaa7e853b629fdafbb0c9543fc394c4aa
Andrew Hart <andrew@datafluency.com>
2012-04-22 18:17:43 -0700

    fix urlencode issue in redirection

--- classes/Auth.class.php
+++ classes/Auth.class.php
@@ -44,8 +44,9 @@ class Auth {
 
     $route = Furnace::GetRequest()->route();
     
-    if (!isset($_SESSION['_auth'])) {
-      Furnace::Redirect($failureRedirect . '?after=' . '/' . urlencode(ltrim($route->raw,'/')));
+    if (!self::test()) {
+      debug($_SESSION);
+      Furnace::Redirect($failureRedirect . '?after=' . urlencode('/' . ltrim($route->raw,'/')));
     } else {
       return true;
     }
